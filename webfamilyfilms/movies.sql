a641c59ed528108f0f79c01d6a96290405fa3c06
Ashley Ardenscor <ashleyardenscor@gmail.com>
2026-07-19 23:13:36 +0200

    Update movies.sql

--- webfamilyfilms/movies.sql
+++ webfamilyfilms/movies.sql
@@ -1,1 +1,12 @@
+------------ ------------ ------------
 
+CREATE TABLE Movies ( -- MovieID, Title, Release, Animanted, Location
+    MovieID int not null,
+    Title nchar(50) not null,
+    Release int not null,
+    Animated nchar(5) not null,
+    Location nchar(10) not null
+);
+
+insert into Movies (Title, Release, Animanted, Location) 
+values
